241bbe18dba165bca78bcaf3fd5b4e1231314118
Jason Pilz <jasonpilz@gmail.com>
2016-03-04 21:32:13 -0700

    implement initial implementation of websockets

--- public/client.js
+++ public/client.js
@@ -1,5 +1,13 @@
-const $options   = $('#options');
-const $addOption = $('#add-option');
+'use strict';
+
+const socket = io();
+
+let $closePoll      = $('#close-poll');
+let $options        = $('#options');
+let $addOption      = $('#add-option');
+let $buttons        = $('#choices :button');
+let $usersConnected = $('#users-connected');
+let pollId          = window.location.pathname.split('/')[2];
 
 $addOption.click((event) => {
   $options.append(
@@ -7,3 +15,30 @@ $addOption.click((event) => {
     <br>`
   )
 })
+
+$closePoll.click(() => {
+  $closePoll.removeClass('btn-danger');
+  $closePoll.addClass('btn-secondary');
+  $closePoll.val('Closed');
+  socket.send('endPoll', {id: pollId });
+});
+
+for (let i = 0; i < $buttons.length; i++) {
+  $buttons[i].addEventListener('click', function () {
+    socket.send('voteCast', { choice: this.value, id: pollId });
+  });
+};
+
+socket.on('usersConnected', (count) => {
+  $usersConnected.text(`Connected Users: ${count}`);
+});
+
+socket.on('voteCount', (votes) => {
+  console.log(votes)
+});
+
+socket.on('disablePoll', () => {
+  console.log("Diable the poll son");
+})
+
+
