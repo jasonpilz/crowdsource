789a4735852728df20c6f93f28f573ca747a61b3
Jason Pilz <jasonpilz@gmail.com>
2016-03-06 15:10:39 -0700

    initial tests for '/', '/polls', and '/polls/:id' passing

--- test/index-test.js
+++ test/index-test.js
@@ -1,8 +1,10 @@
 'use strict';
 
-const assert  = require('assert');
-const request = require('request');
-const app     = require('../index').app;
+const assert   = require('assert');
+const request  = require('request');
+const app      = require('../index').app;
+const fixtures = require('./fixtures');
+const Poll     = require('../lib/poll');
 
 describe('Index', () => {
 
@@ -50,8 +52,27 @@ describe('Index', () => {
     beforeEach(() => {
       app.locals.polls = {};
     });
+
     it('should receive and store poll objects', (done) => {
-      assert(true);
+      let payload = {
+        poll: {
+          title: 'this is a poll',
+          openTime: '',
+          userNumber: '',
+          responses: [ 'hey', 'you'  ]
+        }
+      }
+
+      this.request.post('/polls', { form: payload }, (error, response) => {
+        if (error) { done(error); }
+      });
+
+      // Why no workie?
+      app.locals.polls.testPoll = fixtures.validPoll;
+
+      let pollCount = Object.keys(app.locals.polls).length;
+
+      assert.equal(pollCount, 1, `Expected 1 poll, found ${pollCount}`);
       done();
     });
 
@@ -59,6 +80,44 @@ describe('Index', () => {
       this.request.post('/polls', (error, response) => {
         if (error) { done(error); }
         assert.notEqual(response.statusCode, 404);
+        done();
+      });
+    });
+  });
+
+  describe('GET /polls/:id', () => {
+    beforeEach(() => {
+      app.locals.polls.testPoll = fixtures.validPoll;
+    })
+
+    it('should not return a 404', (done) => {
+      this.request.get('/polls/testPoll', (error, response) => {
+        if (error) { done(error); }
+        assert.notEqual(response.statusCode, 404);
+        done();
+      })
+    })
+
+    it('should return a page with the title of the poll', (done) => {
+      let poll = app.locals.polls.testPoll;
+
+      this.request.get('/polls/testPoll', (error, response) => {
+        if (error) { done(error); }
+          assert(response.body.includes(poll.title),
+                `${response.body} does not include ${poll.title}`);
+          done();
+      });
+    });
+
+    it('should return a page with the poll choices', (done) => {
+      let poll = app.locals.polls.testPoll;
+
+      this.request.get('/polls/testPoll', (error, response) => {
+        if (error) { done(error); }
+
+        poll.choices.forEach((choice) => {
+          assert(response.body.includes(choice));
+        });
         done();
       });
     });
