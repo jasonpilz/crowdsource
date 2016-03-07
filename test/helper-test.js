aa04ecf2b85cf47e4eac3dbff21b781e0308b528
Jason Pilz <jasonpilz@gmail.com>
2016-03-06 19:29:28 -0700

    tests completed for helpers and generate-id

---
+++ test/helper-test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const assert = require('assert');
+const helper = require('../lib/helpers');
+
+describe('The legendary helpers', () => {
+
+  const request = {
+    protocol: 'http',
+    get: (host) => { return 'localhost'; }
+  }
+
+  it('exists', () => {
+    assert(helper);
+  });
+
+  it('can return a voting URL', () => {
+    let voteUrl = helper.setVoteUrl(request, 'asdhq4', '7979798');
+
+    assert(voteUrl);
+    assert.equal('http://localhost/polls/asdhq4', voteUrl);
+  });
+
+  it('can return a admin URL', () => {
+    let adminUrl = helper.setAdminUrl(request, 'adsih', '23234');
+
+    assert(adminUrl);
+    assert.equal('http://localhost/polls/adsih/23234', adminUrl);
+  });
+
+});
--- test/poll-server-test.js
+++ test/poll-server-test.js
@@ -11,7 +11,7 @@ const options = {
 
 describe('Poll Server', () => {
 
-  it('should broadcast user connected to all users', (done) => {
+  it('should broadcast number of connected to all users', (done) => {
     let client1 = io.connect(socketUrl, options);
 
     client1.on('connect', (data) => {
@@ -22,8 +22,49 @@ describe('Poll Server', () => {
       client2.on('connect', (data) => {
         client2.emit('usersConnected', io.engine.clientsCount);
         data.should.equal(2);
+        client2.disconnect();
       });
+
+      data.should.equal(1);
+      client1.disconnect();
+      data.should.equal(0);
     })
     done();
   })
-})
+
+  it('should be able to broadcast messages', (done) => {
+    let client1, client2, client3;
+    let message  = { choice: 'hey', id: '12345134512asdf' }
+    let messages = 0;
+
+    let checkMessage = (client) => {
+      client.on('message', (chnl, msg) => {
+        message.id.should.equal(msg);
+        client.disconnect();
+        messages++;
+        if (messages === 3) {
+          done();
+        }
+      });
+    }
+
+    client1 = io.connect(socketUrl, options);
+    checkMessage(client1);
+
+    client1.on('connect', (data) => {
+      client2 = io.connect(socketUrl, options);
+      checkMessage(client2);
+
+      client2.on('connect', (data) => {
+        client3 = io.connect(socketUrl, options);
+        checkMessage(client3);
+
+        client3.on('connect', (data) => {
+          client2.send('message', message);
+        });
+      });
+    });
+    done();
+  });
+
+});
