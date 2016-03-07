468e9170b7b2560d7e393e3c7bf12feb5355da47
Jason Pilz <jasonpilz@gmail.com>
2016-03-07 10:32:25 -0700

    remove comments

--- index.js
+++ index.js
@@ -108,11 +108,5 @@ function setPollExpiry(poll) {
   }
 }
 
-// if (!module.parent) {
-//   app.listen(app.get('port'), () => {
-//     console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-//   });
-// }
-
 module.exports.app = app;
 module.exports.io  = io;
